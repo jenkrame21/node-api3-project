4a8e3dd3af337cf3fc695a1588574877220bb5ce
Jennifer Kramer <jenkrame@gmail.com>
2021-02-10 14:57:20 -0500

    Connected index file to server.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server.js');
+
+const PORT = 7000;
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on: http://localhost:${PORT}`);
+});
